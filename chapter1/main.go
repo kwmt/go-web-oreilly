18da8f34433655d5bdb005dcaffe5079bc42852e
Yasutaka Kawamoto <kwmt27@gmail.com>
2016-02-07 20:16:57 +0900

    sync.Onceについて説明を記述

--- chapter1/main.go
+++ chapter1/main.go
@@ -2,23 +2,35 @@ package main
 import (
 	"net/http"
 	"log"
+	"sync"
+"text/template"
+	"path/filepath"
 )
+
+
+type templateHandler struct {
+
+	once sync.Once
+	filename string
+	templ *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// テンプレートのコンパイルは1回だけで良いため、
+	// 複数のgoroutineがServeHTTPを呼び出しても、引数として渡した関数が1回しか実行されないこと保証する。
+	// また、本当に必要になるまで処理を後回しにする遅延初期化の役割ももつ。
+	// ただし、エラーが発生しうる処理の場合、エラーに気づきにくくなる問題もあるので、
+	// `template.Must`を使ってグローバル変数に初期化時にセットする方が好まれる場合もある。
+	t.once.Do(func(){
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, nil)
+}
+
 
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-		<html>
-			<head>
-				<title>チャット</title>
-			</head>
-			<body>
-				チャットしましょう！
-			</body>
-		</html>
-		`))
-	})
-
+	http.Handle("/", &templateHandler{filename:"chat.html"})
 
 	if err := http.ListenAndServe(":9000", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
