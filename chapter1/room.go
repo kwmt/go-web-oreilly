35d282a0679623ffd41d29745b2a4473fc47e0c3
Yasutaka Kawamoto <kwmt27@gmail.com>
2016-02-08 13:52:37 +0900

    1.2.3 並行プログラミングで使われるGoのイディオム(select文)

--- chapter1/room.go
+++ chapter1/room.go
@@ -11,3 +11,30 @@ type room struct {
 	// 在室しているすべてのクライアントが保持される
 	clients map[*client]bool
 }
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			// 参加
+			r.clients[client] = true
+		case client := <-r.leave:
+			// 退室
+			delete(r.clients, client)
+			close(client.send)
+		case msg := r.forward:
+			// すべてのクライアントにメッセージを転送
+			for client := range r.clients {
+				select {
+				case client.send <- msg:
+					// メッセージ送信
+				default:
+					// 送信に失敗
+					delete(r.clients, client)
+					close(client.send)
+				}
+			}
+		}
+
+	}
+}
