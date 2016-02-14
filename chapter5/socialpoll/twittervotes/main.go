049676a1f07b92599cdcadfc7eba42f5f89ee20c
Yasutaka Kawamoto <kwmt27@gmail.com>
2016-02-14 17:09:47 +0900

    5.3.2 MongoDBからの読み込み

--- chapter5/socialpoll/twittervotes/main.go
+++ chapter5/socialpoll/twittervotes/main.go
@@ -12,7 +12,45 @@
 
 
 package main
+import (
+	"gopkg.in/mgo.v2"
+	"log"
+)
 
 func main() {
 
+}
+
+var db *mgo.Session
+
+func dialdb() error {
+	var err error
+	log.Println("MongoDBにダイアル中: localhost")
+	db, err = mgo.Dial("localhost")
+	return err
+}
+
+func closedb() {
+	db.Close()
+	log.Println("データベースが閉じられました")
+}
+
+
+type poll struct {
+	Options []string
+}
+
+func loadOptions() ([]string, error) {
+	var options []string
+	iter := db.DB("ballots").C("polls").Find(nil).Iter()
+	var p poll
+	// イテレータで順次アクセス。pollオブジェクトは1つしか使われないから、メモリ使用量を少なくできる
+	// Allメソッドを使うと、投票の数に比例するのでとんでもないメモリの使用量になるかも。
+	// optionsスライスも巨大になることがもちろんある。
+	// このような場合にスケーラビリティを向上させるには、たとえば、複数のtwittervoteプログラムを起動して、処理を分担するといった対策が考えられる。
+	for iter.Next(&p) {
+		options = append(options, p.Options)
+	}
+	iter.Close()
+	return options, iter.Err()
 }
